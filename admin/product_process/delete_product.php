ed5f4c0c0710c8375b6fe4b095e3131d651b6423
Masiss <88313061+Masiss@users.noreply.github.com>
2022-02-22 21:48:49 +0700

    Add files via upload

--- admin/product_process/delete_product.php
+++ admin/product_process/delete_product.php
@@ -12,11 +12,16 @@ if($check->num_rows>0){
 	}
 }
 
+$get_info=mysqli_query($connect,"select * from product where product_id='$id'")->fetch_array();
+$check_product=mysqli_query($connect,"select * from product where product_name='{$get_info['product_name']}' and description='{$get_info['description']}' and product_id!={$get_info['product_id']}");
+if(!empty($check_product)){
+	if($check_product->num_rows==0){
+		$pic=mysqli_query($connect,"select product_image from product where product_id='$id'")->fetch_array()['product_image'];
+		$dir='../pic_product/';
+		unlink($dir.$pic);
+	}
+}
 
-
-$pic=mysqli_query($connect,"select product_image from product where product_id='$id'")->fetch_array()['product_image'];
-$dir='../pic_product/';
-unlink($dir.$pic);
 $sql="delete from product where product_id='$id'";
 mysqli_query($connect,$sql);
 echo "1";
